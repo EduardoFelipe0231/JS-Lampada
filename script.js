0bd262cce10745edbf7e7acaf8c82f9823c0092a
EduardoFelipe0231 <edufelipe.232@hotmail.com>
2025-02-05 01:39:21 -0300

    Update sound

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 let btn = document.getElementById('btn');
 let luz = document.getElementById('luz');
 let onOf = document.getElementById('onOf');
- 
+const audio = new Audio();
+audio.src = "switch.mp3";
+
 
 btn.addEventListener('click', () =>{
-
- 
 
     if(luz.classList.contains('on')){ 
 
         luz.classList.remove('on');
-
         luz.classList.add('off');
-
         document.body.style.backgroundColor = '#FFE5C2';
-
-        onOf.style.backgroundColor = 'rgb(227, 227, 227)';       
+        onOf.style.backgroundColor = 'rgb(227, 227, 227)';        
 
     } else {
 
         luz.classList.remove('off');
-
         luz.classList.add('on');
-
         document.body.style.backgroundColor = '#333333';
-
         onOf.style.backgroundColor = '#00d42e';
-
     }
 
 })
+
+
+console.log(audio);
